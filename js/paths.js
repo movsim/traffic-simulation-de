584224192deb20f4b31415480375132412fe504b
treibi <treiber@vwi.tu-dresden.de>
2017-05-19 11:33:39 +0200

    cleaned paths.js

--- js/paths.js
+++ js/paths.js
@@ -10,11 +10,10 @@ function get_v(dt_afterLC, dt_LC, laneStart, laneEnd){
     return (dt_afterLC>dt_LC) ? laneEnd : laneStart+dv*(laneEnd-laneStart);
 }
 
-// arctan angle with resp to vehicle axis [lane unit cancels out
+// lateral speed in lanes/s (lateral angle=arctan (laneWidth*dvdt/speed)
 
 function get_dvdt(dt_afterLC, dt_LC, laneStart, laneEnd, speed){
     var acc_v=4./(dt_LC*dt_LC);
     var dt=(dt_afterLC<0.5*dt_LC) ? dt_afterLC : dt_LC-dt_afterLC;
-    var dv = (dt_afterLC<0.5*dt_LC) ? 0.5*acc_v*dt*dt : 1- 0.5*acc_v*dt*dt;
     return (dt_afterLC>dt_LC) ? 0 : acc_v*dt*(laneEnd-laneStart);
 }
