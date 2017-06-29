628fe2c38938f06306eb8179a2f688f43644de21
treibi <treiber@vwi.tu-dresden.de>
2017-06-29 20:29:28 +0200

    final layout landscape/portrait for bigger screens

---
+++ js/demo_initializeMicro.js
@@ -0,0 +1,12 @@
+
+//!! test Micro-IC (!! trucks may change to cars due to init truck frac)
+
+if(false){
+    types  =[0,    0,    1,    0];
+    lengths=[8,    5,    14,   7];
+    widths =[4.5,  4,    6,  4.5];
+    longPos=[150,  160,  170,  180];
+    lanes  =[0,    1,    2,    0];
+    speeds =[25,   25,   0,   30];
+    mainroad.initializeMicro(types,lengths,widths,longPos,lanes,speeds);
+}
